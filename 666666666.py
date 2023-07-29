68d64eb9637c98944b081346a268dec0ed0f6fc8
Shamim-joy <133912042+Shamim-joy@users.noreply.github.com>
2023-07-29 12:58:15 +0600

    Add files via upload

---
+++ 666666666.py
@@ -0,0 +1,36 @@
+def MUEORðŸ¤¬_TRICKER():
+	os.system('clear')
+	print(logo)
+	os.system('xdg-open https://www.facebook.com/profile.php?id=100007946797233')
+	print('\x1b[1;92m[1]\x1b[1;93m Random UID Cloning')
+	print('\x1b[1;92m[0]\x1b[1;94m Back')
+	print(47*'\033[92;1m-')
+	print ("")
+	opt = input('\x1b[1;93m[+] Choose option: \x1b[1;92m')
+	if opt =='1':
+		method()
+	elif opt =='0':
+		main()
+	else:
+		print('\n\033[1;31mChoose valid option\033[0;97m')
+		MUEORðŸ¤¬_KING()
+		
+def method():
+	os.system('clear')
+	print(logo)
+	os.system('xdg-open https://www.facebook.com/profile.php?id=100007946797233')
+	print('\x1b[1;92m[1]\x1b[1;93m Method1  \x1b[1;92m[Ok idz] \x1b[1;93m[BEST]')
+	print('\x1b[1;92m[2]\x1b[1;94m Method2  \x1b[1;92m[Ok idz]')
+	print('\x1b[1;92m[0]\x1b[1;95m Back')
+	print(47*'\033[92;1m-')
+	print ("")
+	opt = input('\x1b[1;93m[+] Choose option: \x1b[1;92m')
+	if opt =='1':
+		method1()
+	elif opt =='2':
+		method2()
+	elif opt =='0':
+		MUEORðŸ¤¬_KING()
+	else:
+		print('\n\033[1;31mChoose valid option\033[0;97m')
+		method()
